b68514ea8137182770020733c84979fccac201bd
Seun1601 <114428457+Seun1601@users.noreply.github.com>
2023-01-04 23:22:35 +0100

    Added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+    
+      colors: { 
+        primary: { 500: '#FF6363;', 800: '#FF1313;', 
+        } 
+      }, 
+      fontFamily: { 
+        poppins: ['Poppins', 'sans-serif'],     
+      CursedTimerUlil: ['CursedTimerUlil', 'cursive'], 
+    }, 
+  },
   },
   plugins: [],
 }
